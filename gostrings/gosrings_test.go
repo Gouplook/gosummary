5b2c5025b41eae5ababe4ffe6c1729d7eccfa931
Yinjinlin <jinlin.yin@uisee.com>
2022-12-09 17:37:31 +0800

    feat: ..

--- gostrings/gosrings_test.go
+++ gostrings/gosrings_test.go
@@ -1,7 +1,26 @@
 package gostrings
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestGoStrings2(t *testing.T) {
 	GoStrings2()
+	s := "e200.situ.car9"
+	index := strings.Index(s, ".") // 4
+
+	str := s[0:index]
+	switch str {
+	case "e100":
+		fmt.Println("A")
+	case "e200":
+		fmt.Println("b")
+	default:
+		fmt.Println("....")
+	}
+
+	fmt.Println(str)
+
 }
